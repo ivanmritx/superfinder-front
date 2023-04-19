eb87ee9883adf14fa3ed9162a2b71bc348a63c0e
Iván Mariño <ivanmr@ext.inditex.com>
2023-04-19 12:07:32 +0200

    Updated header styles and SEO info

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
 return (
 <div className="header">
     <h1>Super más barato</h1>
-    <h2>Busca y compara los precios de productos en distintas cadenas de supermercados de España.</h2>
+    <h2>Busca y compara los precios de productos en distintas cadenas de supermercados de España. Encuentra los productos al precio más barato y ahorra dinero en tu próxima lista de la compra.</h2>
 </div>);
 }
 
